81c008d7bbc3c7c9b11848e49a956437ee0809b6
AniaXS <anna.m.stefanska@gmail.com>
2019-03-31 08:16:29 +0200

    Third Tasks Commit

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,11 +2,22 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
+
+    @Column(name = "name")
     String title;
+
+    @Column(name = "description")
     String content;
 }
